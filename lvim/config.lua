9cad31648343edc1cb1b33e1773c351a112e6956
nickbar01234 <nickbar01234@gmail.com>
2023-08-19 17:22:53 +0700

    Backup /Users/nickbar01234/.config/lvim/config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -67,7 +67,7 @@ lvim.builtin.which_key.mappings["zh"] = {
 vim.api.nvim_create_user_command('Header',
   function ()
     local commentstring = vim.api.nvim_buf_get_option(0, "commentstring")
-    local symbol = vim.split(commentstring, '%s')[0]
+    local symbol = vim.split(commentstring, '%s')
     print(commentstring)
     print(symbol)
   end,
