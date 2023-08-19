4c1597b19e7def0e3b718f89380554287ad9345d
nickbar01234 <nickbar01234@gmail.com>
2023-08-19 21:52:18 +0700

    Backup /Users/nickbar01234/.config/lvim/lua-utils/array.lua

--- lua-utils/array.lua
+++ lua-utils/array.lua
@@ -4,9 +4,9 @@ local Array = {}
 
 -- Returns true if array contains value and otherwise false
 --
--- @param array any[]
--- @param value any
--- @return boolean
+---@param array any[]
+---@param value any
+---@return boolean
 function Array.contains(array, value)
   for _, v in ipairs(array) do
     if v == value then
