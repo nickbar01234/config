fac6e9e9812fb787a5827b16ebf7917a58c781db
nickbar01234 <nickbar01234@gmail.com>
2023-08-19 20:40:43 +0700

    Backup /Users/nickbar01234/.config/lvim/config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,7 +4,7 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
-vim.api.nvim_command("set runtimepath^=~/.config/lvim/")
+vim.api.nvim_command("set runtimepath^=~/.config/lvim/utils.lua")
 local Array = require "utils.lua"
 
 lvim.colorscheme = "monokai_pro"
