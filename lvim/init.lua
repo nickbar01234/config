4015b999e5ea6e95e8f4eefd470a8289f4e5a1a1
nickbar01234 <nickbar01234@gmail.com>
2023-08-24 15:52:16 +0700

    Backup /Users/nickbar01234/.config/lvim/init.lua

--- lvim/init.lua
+++ lvim/init.lua
@@ -1,3 +1,6 @@
+-- TODO(nickbar01234): Refactor to a different folder
+vim.opt.runtimepath:append(",/Users/nickbar01234/.config/lvim/lua-utils")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
